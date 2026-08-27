71e02635688c53cb6564734d23d219219f241847
Adonis Mioi <Adonis1mioi@gmail.com>
2026-08-27 16:19:00 -0600

    first commit

---
+++ age_guess.py
@@ -0,0 +1,16 @@
+import random
+
+def guess_my_age():
+   name= input("Hey! What's your name?")
+   input(f"Hello {name}, im Rotee and i'm going to guess your age!") 
+   while True:
+       guess = random.randint(15, 40)
+       answer = input(f"Are you {guess} years old?(y/n):").strip().lower()
+
+       if answer == 'y': 
+           print(f"YOU LIKE THAT! YOU LIKE THAT!{name} is {guess} years old.")
+           break
+       elif answer =='n':
+           print("Rats.")
+
+guess_my_age()
